d8e6e1570437fccd665cf95c6c8030abca5e247d
Matt Kraatz <matt.kraatz@gmail.com>
2016-10-30 12:17:37 -0500

    updated Google sign-in method to Redirect

--- NashvilleHistory/www/js/controllers/AppCtrl.js
+++ NashvilleHistory/www/js/controllers/AppCtrl.js
@@ -46,7 +46,7 @@ app.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
   // Google Login
   $scope.google = function() {
     var provider = new firebase.auth.GoogleAuthProvider();
-    firebase.auth().signInWithPopup(provider)
+    firebase.auth().signInWithRedirect(provider)
     .then(function(result) {
       // This gives you a Google Access Token. You can use it to access the Google API.
       var token = result.credential.accessToken;

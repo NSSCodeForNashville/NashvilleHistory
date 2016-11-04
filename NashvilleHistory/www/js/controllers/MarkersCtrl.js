17b5b567659d4131f4efa2e21167caa256afcc31
Delaine Wendling <delaine.wendling@gmail.com>
2016-11-03 22:01:06 -0500

    Added a marker to indicate where the user is

--- NashvilleHistory/www/js/controllers/MarkersCtrl.js
+++ NashvilleHistory/www/js/controllers/MarkersCtrl.js
@@ -16,7 +16,17 @@ app.controller('MarkersCtrl', function($scope, $state, $cordovaGeolocation, Mark
     },
     zoom: 10
   }
-
+  $scope.youAreHere = {
+    id: 0,
+    coords: {
+      latitude: 36.1637,
+      longitude: -86.7816
+    },
+    options: {
+      icon: '../../img/greenMarker.png'
+    }
+  }
+  //The following code block uses the Cordova Geolocation Plugin to access the user's native Geolocation technology within his/her device and find his/her location.
   let posOptions = {timeout: 10000, enableHighAccuracy: false};
   $cordovaGeolocation
     .getCurrentPosition(posOptions)
@@ -26,16 +36,27 @@ app.controller('MarkersCtrl', function($scope, $state, $cordovaGeolocation, Mark
         center: {latitude: position.coords.latitude, longitude: position.coords.longitude },
         zoom: 12
       };
+      $scope.youAreHere = {
+        id: 0,
+        coords: {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude
+        },
+        options: {
+          icon: '../../img/greenMarker.png'
+        }
+      }
+      //Once the location of the user is found using geolocation, this function is called to find the markers that are within a certain radius of the user's location.
       getMarkersWithinRadius();
     }, function(err) {
       console.log("Could not get location");
     });
 
-
     function getMarkersWithinRadius(){
       lat = $scope.map.center.latitude.toString();
       long = $scope.map.center.longitude.toString();
-      MarkerCardsFact.getMarkersInRadius(lat, long, "6000")
+      //The function called in the factory uses the Historic Markers API to find the markers within a certain radius. This function takes the latitude and longitude of the user (as strings) as well as the desired radius (in meters) as arguments.
+      MarkerCardsFact.getMarkersInRadius(lat, long, "3500")
       .then((data)=>{
         console.log("markers in area", data);
         HistoricalMarkers = data;
@@ -43,6 +64,7 @@ app.controller('MarkersCtrl', function($scope, $state, $cordovaGeolocation, Mark
       })
     }
 
+    //The purpose of this function is to take the latitude and longitude of each marker, found by the getMarkersInRadius function above, and find the distance from the user to that marker. This function uses a function in the factory to make a call to Google Maps Distance Matrix API.
     function addDistanceToMarkers(){
       return $q.all(
         HistoricalMarkers.map((marker)=>{
@@ -50,7 +72,7 @@ app.controller('MarkersCtrl', function($scope, $state, $cordovaGeolocation, Mark
         })
       )
       .then((data)=>{
-        // console.log("data about distance", data);
+        console.log("data about distance", data);
         let distanceData = data.map((row)=>{
           return {
             distance: parseFloat(row.rows[0].elements[0].distance.text.split(" ")[0]),
@@ -66,6 +88,7 @@ app.controller('MarkersCtrl', function($scope, $state, $cordovaGeolocation, Mark
       })
     }
 
+    //The next two functions sort the markers in the given radius from the closest to the furthest away from the user.
     function sortMarkersByDistance(){
       $scope.HistoricalCards = sortByKey(HistoricalMarkers, "distance");
     }
@@ -77,34 +100,23 @@ app.controller('MarkersCtrl', function($scope, $state, $cordovaGeolocation, Mark
       });
     }
 
-        //     bounds: {},
-        // control: {},
-        // pan: true
-
-
-  // var watchOptions = {
-  //   timeout : 3000,
-  //   enableHighAccuracy: false // may cause errors if true
-  // };
+  //The following code block watches the user's location and updates the center of the map as the user moves.
+  // var watchOptions = { timeout : 5000, enableHighAccuracy: false };
 
   // var watch = $cordovaGeolocation.watchPosition(watchOptions);
   // watch.then(
   //   null,
   //   function(err) {
-  //     // error
+  //     console.log("New Location Not Found");
   //   },
   //   function(position) {
-  //     var lat  = position.coords.latitude
-  //     var long = position.coords.longitude
+  //     $scope.map = {
+  //       center: {latitude: position.coords.latitude, longitude: position.coords.longitude },
+  //       zoom: 12
+  //     };
   // });
 
 
   // watch.clearWatch();
-  // // OR
-  // $cordovaGeolocation.clearWatch(watch)
-  //   .then(function(result) {
-  //     // success
-  //     }, function (error) {
-  //     // error
-  //   });
+
 })
